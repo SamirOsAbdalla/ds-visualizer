5318c42d1dde3c0dad6285c532bf84c25a02bfa8
samir01 <samir.os.abdalla@gmail.com>
2023-09-18 19:41:24 -0700

    Created components folder to house react components

---
+++ src/components/Title/Title.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import "./Title.css"
+
+const Title = () => {
+    return (
+        <div className="title__wrapper">
+            Data Structure Visualizer
+        </div>
+    )
+}
+
+export default Title
