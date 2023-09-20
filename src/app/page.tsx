060921b902a7b710ed5c9367114bd701464f21f4
samir01 <samir.os.abdalla@gmail.com>
2023-09-19 21:53:46 -0700

    Added linked list option to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,14 @@ import Image from 'next/image'
 import "./page.css"
 import Title from "../components/Title/Title"
 import DsChooser from '@/components/DsChooser/DsChooser'
-
+import LinkedListDs from '@/components/DataStructures/LinkedList/LinkedList'
 
 export default function Home() {
   return (
     <main className="home__wrapper">
       <Title />
       <DsChooser />
+      <LinkedListDs />
     </main>
   )
 }
